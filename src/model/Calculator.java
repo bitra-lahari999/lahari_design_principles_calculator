8f810562ac2f25d000c398c13d63fa7568e5a1f2
lahari <bitralahari20171999@gmail.com>
2020-02-10 22:42:43 +0530

    My project commit

--- src/model/Calculator.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public interface Calculator {
-
-    public double addition();
-    public double subtraction();
-    public double multiplication();
-    public double division();
-}
